16f23578d0153ed50c9f134bb22accd814e3a4a8
JunaidLatif <junaidlatif004@gmail.com>
2022-10-14 15:42:36 +0500

    Bug Resolved

--- Client/src/App.jsx
+++ Client/src/App.jsx
@@ -3,9 +3,9 @@ import { Routes, Route } from "react-router-dom";
 
 // components 
 import Login from "./Components/Register/Login/Login"
-import SignUp from "./Components/Register/Signup/Signup"
-import Password from "./Components/Register/Signup/Component/Password/Password";
-import Account from "./Components/Register/Signup/Component/Accounts/Account";
+import SignUp from "./Components/Register/SignUp/Signup"
+import Password from "./Components/Register/SignUp/Component/Password/Password";
+import Account from "./Components/Register/SignUp/Component/Accounts/Account";
 import Home from "./Components/Home/Home";
 
 // css 
